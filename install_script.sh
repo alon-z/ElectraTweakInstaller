453f8d6b5936575743f6ff827a5eec0e8b647139
Alon Zusman <aloniko.zus@gmail.com>
2018-01-19 18:51:50 +0200

    Final fixes

--- install_script.sh
+++ install_script.sh
@@ -1,26 +1,29 @@
 # Create dir for the tweak
-mkdir $1.dir
+mkdir  -p $1.dir
 OLD_PWD=$pwd
 
 # Extract
-tar -xvf $1 -C $1.dir/
+#tar -xvf $1 -C $1.dir/
 cd $1.dir
+ar x ../$1
 tar -xvf data.tar.*
 cd Library/
 
 # Sing using ldid (aka ldid2)
-for bin in $(for file in $(find . | grep -Ev ".*.png" | grep -Ev ".*.plist" | grep -Ev ".*.txt" | grep -Ev ".*.sh"); do file $file; done | grep -v directory | awk '{print $1}' | sed 's@:@@' | sort -u); do echo $bin; ldid -S "$bin"; done
+# for bin in $(for file in $(find . | grep -Ev ".*.png" | grep -Ev ".*.plist" | grep -Ev ".*.txt" | grep -Ev ".*.sh"); do file $file; done | grep -v directory | awk '{print $1}' | sed 's@:@@' | sort -u); do echo $bin; ldid -S "$bin"; done
 
 # Copy to device
 if find . | grep MobileSubstrate; then
-scp MobileSubstrate/DynamicLibraries/* root@$2:/bootstrap/Library/SBInject/.
+scp -P 2222 MobileSubstrate/DynamicLibraries/* root@$2:/bootstrap/Library/SBInject/.
 fi
 if find . | grep PreferenceBundles; then
-scp -r PreferenceBundles/*.bundle root@$2:/bootstrap/Library/PreferenceBundles/.
-scp -r PreferenceLoader/Preferences/*.plist root@$2:/bootstrap/Library/PreferenceLoader/Preferences/.
+scp -P 2222 -r PreferenceBundles/*.bundle root@$2:/bootstrap/Library/PreferenceBundles/.
+scp -P 2222 -r PreferenceLoader/Preferences/*.plist root@$2:/bootstrap/Library/PreferenceLoader/Preferences/.
 fi
 if find . | grep "Application Support"; then
-scp -r "./Application Support/*" root@10.0.0.12:"/Library/Application\ Support/."
+cd "Application Support"
+scp -P 2222 -r * root@$2:"/Library/Application\ Support/."
+cd ..
 fi
 left=$(find . | grep -v MobileSubstrate | grep -v Preference | grep -v "Application Support" | tail -n +2)
 echo "Left overs:"
--- search_and_download_tweak.sh
+++ search_and_download_tweak.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 TWEAKS=$(curl --silent https://sheets.googleapis.com/v4/spreadsheets/1YptWW_bBdEQ9naYAfiZ2Aj4H93Y56I4xSYI29u4q_-Y/values/A21:A?key=AIzaSyDig3bllvpnIql6jhjvvVQ5J4672DUsMdI)
 LINE=$(echo "$TWEAKS" | grep -ni $1)
+NUM_LINES=$(echo "LINE" | wc -l)
+if [[ "$NUM_LINES" != 1 ]]; then
+  echo "Found more than one resault: "
+  echo "$LINE"
+  exit
+fi
 if [ "$LINE" ]; then
 LINE=$(echo "$LINE" | awk '{ print $1 }' | sed 's@:@@')
 LINE=$((($LINE-5)/3+22))
@@ -11,6 +17,13 @@ TWEAK_STATUS=$(echo "$STATUS" | tail -n 1)
 echo "Tweak name: $TWEAK_NAME"
 echo "Status: $TWEAK_STATUS"
 echo "Description: $TWEAK_DESC"
+if [[ "$TWEAK_STATUS" != "Stable/Works" ]]; then
+  echo "The tweak status is not stable, are you sure you would like to install?"
+  read -p 'Y for yes: ' answer
+  if [[ "$answer" != Y ]]; then
+    exit
+  fi
+fi
 else
 echo "Not found on electra sheet (reddit)"
 echo "Do you still want to search on cydiaupdates for deb?"
@@ -22,15 +35,25 @@ fi
 if [ -z "$TWEAK_NAME" ]; then
 TWEAK_NAME=$1
 fi
-CYDIA_UPDATES=$(curl --silent "https://www.cydiaupdates.org/search/?s=$TWEAK_NAME&section=all&repo=all" | grep pack)
+CYDIA_UPDATES=$(curl --silent "https://www.cydiaupdates.org/search/?s=$TWEAK_NAME&section=Tweaks&repo=all" | grep pack)
 CYDIA_UPDATES=$( echo "$CYDIA_UPDATES" | grep -i $TWEAK_NAME)
 if [ -z "$CYDIA_UPDATES" ]; then
 echo "Tweak not found on cydia updates"
 exit
+fi
+NUM_LINES=$(echo "$CYDIA_UPDATES" | wc -l)
+if [[ "$NUM_LINES" != "1" ]]; then
+  echo "Found more than one tweak that matches $TWEAK_NAME or $1 on cydia updates"
+  echo "$CYDIA_UPDATES" | grep class=\"media-heading\" -n | sed 's@<.*><a.*\">@@' | sed 's@</a> <small>@ @' | sed 's@<.*@@'
+  read -p "Choose the right tweak: " TWEAK_NUM
+  CYDIA_UPDATES=$(echo "$CYDIA_UPDATES" | grep class=\"media-heading\" | sed "${TWEAK_NUM}q;d")
 fi
 TWEAK_VERSION=$(echo "$CYDIA_UPDATES" | awk '{ print $4 }' | sed 's@<small>@@' | sed 's@</.*@@')
 echo Version: $TWEAK_VERSION
 #https://www.cydiaupdates.org/pack/152802/
 PACK=$(echo "$CYDIA_UPDATES" | awk '{ print $3 }' | sed 's@.*\"/@@' | sed 's@\".*@@')
 DEB=$(curl --silent "https://www.cydiaupdates.org/$PACK" | grep deb | grep -v \<meta | head -n 1 | sed 's@.*href=\"@@' | sed 's@\">.*@@')
-curl --silent "$DEB" > "$( echo $DEB | sed 's@.*/@@' )"
+echo "Downloading..."
+echo $DEB
+#curl --silent "$DEB" > "$( echo $DEB | sed 's@.*/@@' )"
+wget $DEB
