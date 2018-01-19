887f07f9cb052d51be1199615cd06db3bc4b1054
alon zusman <aloniko.zus@gmail.com>
2018-01-19 15:23:11 +0200

    Added some ifs

--- search_and_download_tweak.sh
+++ search_and_download_tweak.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 TWEAKS=$(curl --silent https://sheets.googleapis.com/v4/spreadsheets/1YptWW_bBdEQ9naYAfiZ2Aj4H93Y56I4xSYI29u4q_-Y/values/A21:A?key=AIzaSyDig3bllvpnIql6jhjvvVQ5J4672DUsMdI)
-AMOUNT=$(echo "$TWEAKS" | wc -l)
-LINE=$(echo "$TWEAKS" | grep -ni $1 | awk '{ print $1 }' | sed 's@:@@')
+LINE=$(echo "$TWEAKS" | grep -ni $1)
+if [ "$LINE" ]; then
+LINE=$(echo "$LINE" | awk '{ print $1 }' | sed 's@:@@')
 LINE=$((($LINE-5)/3+22))
 STATUS=$(curl --silent https://sheets.googleapis.com/v4/spreadsheets/1YptWW_bBdEQ9naYAfiZ2Aj4H93Y56I4xSYI29u4q_-Y/values/A$LINE:C$LINE?key=AIzaSyDig3bllvpnIql6jhjvvVQ5J4672DUsMdI | grep -v \"values\" | grep "\[" -A 3 | tail -n 3 | sed 's@[",]@@g' | sed 's@[ ]*@@')
 TWEAK_NAME=$(echo "$STATUS" | head -n 1)
@@ -10,7 +11,23 @@ TWEAK_STATUS=$(echo "$STATUS" | tail -n 1)
 echo "Tweak name: $TWEAK_NAME"
 echo "Status: $TWEAK_STATUS"
 echo "Description: $TWEAK_DESC"
-CYDIA_UPDATES=$(curl --silent "https://www.cydiaupdates.org/search/?s=$TWEAK_NAME&section=all&repo=all" | grep pack | grep -i $TWEAK_NAME)
+else
+echo "Not found on electra sheet (reddit)"
+echo "Do you still want to search on cydiaupdates for deb?"
+read -p 'Y for yes: ' answer
+if [[ "$answer" != Y ]]; then
+exit
+fi
+fi
+if [ -z "$TWEAK_NAME" ]; then
+TWEAK_NAME=$1
+fi
+CYDIA_UPDATES=$(curl --silent "https://www.cydiaupdates.org/search/?s=$TWEAK_NAME&section=all&repo=all" | grep pack)
+CYDIA_UPDATES=$( echo "$CYDIA_UPDATES" | grep -i $TWEAK_NAME)
+if [ -z "$CYDIA_UPDATES" ]; then
+echo "Tweak not found on cydia updates"
+exit
+fi
 TWEAK_VERSION=$(echo "$CYDIA_UPDATES" | awk '{ print $4 }' | sed 's@<small>@@' | sed 's@</.*@@')
 echo Version: $TWEAK_VERSION
 #https://www.cydiaupdates.org/pack/152802/
